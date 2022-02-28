56029d52bf5e20d9371c7ae8ff48664460c51223
Surajit Karuri <surajitkaruri@gmail.com>
2022-02-28 22:25:26 +0530

    dynamic color for rating

--- src/components/RatingDash.jsx
+++ src/components/RatingDash.jsx
@@ -13,10 +13,31 @@ function RatingDash({ ratings }) {
     { _id: 0, count: 0 }
   );
 
+  const ratingColor = (point) => {
+    switch (Math.trunc(point)) {
+      case 5:
+        return 'text-green-700';
+      case 4:
+        return 'text-lime-700';
+      case 3:
+        return 'text-amber-600';
+      case 2:
+        return 'text-orange-700';
+      case 1:
+        return 'text-red-600';
+      default:
+        return 'text-gray-600';
+    }
+  };
+
   return (
     <div className="flex gap-6 my-4">
       <div>
-        <h1 className="text-6xl font-semibold text-fuchsia-900">
+        <h1
+          className={`text-6xl font-semibold ${ratingColor(
+            ratingConsolidate._id
+          )}`}
+        >
           {ratingConsolidate._id}
         </h1>
         <div className="flex items-center justify-between text-fuchsia-600 mt-3">
