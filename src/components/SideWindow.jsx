48feb1b57c8755bb8222f3e7bc46da3a9fbdaa98
Surajit Karuri <surajitkaruri@gmail.com>
2022-03-18 21:44:09 +0530

    fix: showRecipe user recipe fetch

--- src/components/SideWindow.jsx
+++ src/components/SideWindow.jsx
@@ -8,23 +8,26 @@ import Loader from './Loader';
 function SideWindow({ authorId, fullName }) {
   const [{ data, isLoading, error }] = useFetchData(
     `${API}/recipe?user=${authorId}`,
-    []
+    {
+      recipes: [],
+      count: 0,
+    }
   );
 
   return (
-    <div className="reviews bg-fuchsia-50/50 rounded-md shadow-lg relative">
-      <h4 className="bg-fuchsia-50/50 text-fuchsia-900 py-2 pl-4 text-lg font-semibold rounded-t-md">
+    <div className='reviews bg-fuchsia-50/50 rounded-md shadow-lg relative'>
+      <h4 className='bg-fuchsia-50/50 text-fuchsia-900 py-2 pl-4 text-lg font-semibold rounded-t-md'>
         More from {fullName}
       </h4>
       {isLoading ? (
-        <div className="text-center flex justify-center">
+        <div className='text-center flex justify-center'>
           <Loader isLoading={isLoading} />
         </div>
       ) : error ? (
-        <h3 className="error-card">{error}</h3>
+        <h3 className='error-card'>{error}</h3>
       ) : (
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] gap-2 px-2 py-2">
-          {data.map((recipe, index) => (
+        <div className='grid grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] gap-2 px-2 py-2'>
+          {data.recipes.map((recipe, index) => (
             <React.Fragment key={index}>
               <Link
                 to={`/recipe/${recipe?._id}`}
